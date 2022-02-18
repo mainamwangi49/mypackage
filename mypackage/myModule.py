c18805b4f6cbf4690ff98a66bdf701466d22a131
mainamwangi49 <erichmwangi@gmail.com>
2022-02-18 12:58:33 +0300

    First Commit

---
+++ mypackage/myModule.py
@@ -0,0 +1,24 @@
+# Function that returns the top n items in an array in descending order
+def top_n(items, n):
+    """Return the top n items in an array in desc order
+
+    Args:
+    items(array): list or array-like object
+    n(int): num of items to return
+
+    Return:
+    array: top n items, in desc order
+
+    Egs:
+    >>>top_n([8,3,2,7,4],3)
+    [8,7,4]
+    """
+    for i in range(n): # keep sorting until we have the top n items
+        for j in range(len(items)-1-i):
+
+            if items[j] > items[j+1] # if this item is bigger than the next item...
+                items[j], items[j+1] = items[j+1], items[j] # swap the two!
+        # get the last n items
+        top_n = items[-n]:
+        # return in descending order
+        return top_n[::-1:]        
